512ae3ca9bb70d8713b7d8837dc4a2b1bcadb3de
tomerco25 <tomerco25@gmail.com>
2016-07-18 20:38:54 +0300

    help file

---
+++ loops.py
@@ -0,0 +1,26 @@
+# #
+# counter=0
+# for i in range(10):
+#     counter+=1
+#     print "the value of counter is:%s" %counter
+#     print "the value of i is:%s" %i
+#
+# counter=0
+# for j in range(5,10):
+#     counter+=1
+#     print "the value of counter is:%s" %counter
+#     print "the value of j is:%s" %j
+#
+# counter=0
+# for i in "aavvvdw":
+#    counter+=1
+#    print i
+#    print "-----"
+#    print counter
+#
+# fruits = ['banana', 'apple',  'mango']
+# print range(len(fruits))
+# for index in range(len(fruits)):
+#    print 'Current fruit :', fruits[index]
+#    print index
+
