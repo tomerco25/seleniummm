3a16f1818f45f5b8ac33cb60b4aba1490d434a92
tomerco25 <tomerco25@gmail.com>
2016-07-21 22:17:45 +0300

    help file

--- loops.py
+++ loops.py
@@ -1,44 +1,44 @@
-"----------------------------------------------------------"
-# counter=0
-# for i in range(10):
-#     counter+=1
-#     print "the value of counter is:%s" %counter
-#     print "the value of i is:%s" %i
-"----------------------------------------------------------"
-# counter=0
-# for j in range(5,10):
-#     counter+=1
-#     print "the value of counter is:%s" %counter
-#     print "the value of j is:%s" %j
-"----------------------------------------------------------"
-# counter=0
-# for i in "aavvvdw":
-#    counter+=1
-#    print i
-#    print "-----"
-#    print counter
-"----------------------------------------------------------"
-# fruits = ['banana', 'apple',  'mango']
-# print range(len(fruits))
-# for index in range(len(fruits)):
-#    print 'Current fruit :', fruits[index]
-#    print index
-"----------------------------------------------------------"
-"""reveese string"""
-str = ''
-for letter in "hello":
-    str = letter + str
-print str
-"----------------------------------------------------------"
-"""find the second max"""
-tom=[3,5,18,1,9,10,7,2,15,6,2,7,22]
-max=0
-secondmax=0
-for i in range(len(tom)):
-    if tom[i]>max:
-        secondmax=max
-        max=tom[i]
-    elif tom[i] > secondmax:
-        secondmax = tom[i]
-print secondmax
-print max
+# "----------------------------------------------------------"
+# # counter=0
+# # for i in range(10):
+# #     counter+=1
+# #     print "the value of counter is:%s" %counter
+# #     print "the value of i is:%s" %i
+# "----------------------------------------------------------"
+# # counter=0
+# # for j in range(5,10):
+# #     counter+=1
+# #     print "the value of counter is:%s" %counter
+# #     print "the value of j is:%s" %j
+# "----------------------------------------------------------"
+# # counter=0
+# # for i in "aavvvdw":
+# #    counter+=1
+# #    print i
+# #    print "-----"
+# #    print counter
+# "----------------------------------------------------------"
+# # fruits = ['banana', 'apple',  'mango']
+# # print range(len(fruits))
+# # for index in range(len(fruits)):
+# #    print 'Current fruit :', fruits[index]
+# #    print index
+# "----------------------------------------------------------"
+# """reveese string"""
+# str = ''
+# for letter in "hello":
+#     str = letter + str
+# print str
+# "----------------------------------------------------------"
+# """find the second max"""
+# tom=[3,5,18,1,9,10,7,2,15,6,2,7,22]
+# max=0
+# secondmax=0
+# for i in range(len(tom)):
+#     if tom[i]>max:
+#         secondmax=max
+#         max=tom[i]
+#     elif tom[i] > secondmax:
+#         secondmax = tom[i]
+# print secondmax
+# print max
