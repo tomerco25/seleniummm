405e14ad3dadbdd6922a33f10e42013167c46b75
tomerco25 <tomerco25@gmail.com>
2016-08-10 19:48:13 +0300

    help file

--- mthreads.py
+++ mthreads.py
@@ -6,7 +6,7 @@ wb = load_workbook(filename='C:/1/units.xlsx')
 sheet_ranges = wb['Sheet1']
 tlock=threading.Lock()
 def printexcel():
-    global  count
+    global count
     tlock.acquire() #also we can use with tlock: instead acquire&release(see below)
     print "------Starting {}------".format(threading.current_thread().name)
     tlock.release()
@@ -14,8 +14,9 @@ def printexcel():
         with tlock:
             count=str(count)
             unitid=sheet_ranges['A'+count+''].value
-            print (threading.current_thread().name),"UnitId is:{}".format(unitid)
-            count = int(count) + 1
+            if unitid!=None:
+                print (threading.current_thread().name),"UnitId is:{}".format(unitid)
+                count = int(count) + 1
 for i in range(10):
     t=threading.Thread(target=printexcel)
     t.start()
