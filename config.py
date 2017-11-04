4ec1aef87d445107371131b2ae5eb3f6f7e902fa
tomerco25 <tomerco25@gmail.com>
2017-11-04 12:52:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ wallaurl="http://www.walla.co.il/"
 passurl="https://www.passpack.com/online/"
 bbcurl="http://www.bbc.com/news/world-europe-36950521"
 checkboxurl="http://semantic-ui.com/modules/checkbox.html"
+
